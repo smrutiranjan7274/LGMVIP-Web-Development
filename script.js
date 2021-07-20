91df3a735129edbf9826d2e8643e41713bebb690
smrutiranjan7274 <67579934+smrutiranjan7274@users.noreply.github.com>
2021-07-21 02:53:10 +0530

    Add Sections

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ counter++;
 if (counter > 4) {
   counter = 1;
 }
-For Automatic Carousel Control
+// For Automatic Carousel Control
 setInterval(function () {
   document.getElementById("radio" + counter).checked = true;
   counter++;
@@ -14,3 +14,18 @@ setInterval(function () {
     counter = 1;
   }
 }, 5000);
+
+//Auto-play/pause video om Mouse Hover
+document.getElementById("vid1").addEventListener("mouseover", function () {
+  this.play();
+});
+document.getElementById("vid1").addEventListener("mouseleave", function () {
+  this.pause();
+});
+
+document.getElementById("vid2").addEventListener("mouseover", function () {
+  this.play();
+});
+document.getElementById("vid2").addEventListener("mouseleave", function () {
+  this.pause();
+});

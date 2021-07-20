6dba49dc8453c23b50622b3b0f07d7076620f2ac
smrutiranjan7274 <67579934+smrutiranjan7274@users.noreply.github.com>
2021-07-20 21:03:58 +0530

    Initial Commit

---
+++ WebDev/Task-1/JS/script.js
@@ -0,0 +1,16 @@
+// Carousel Control
+var counter = 1;
+//Manual Carousel Control
+document.getElementById("radio" + counter).checked = true;
+counter++;
+if (counter > 4) {
+  counter = 1;
+}
+// For Automatic Carousel Control
+// setInterval(function () {
+//   document.getElementById("radio" + counter).checked = true;
+//   counter++;
+//   if (counter > 4) {
+//     counter = 1;
+//   }
+// }, 5000);
